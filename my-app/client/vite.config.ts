16d8d1384cb19b427117ef15754962f764c16e22
llelez <zhaolelellll@gmail.com>
2024-11-09 19:05:45 -0800

    setup testing

--- my-app/client/vite.config.ts
+++ my-app/client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { configDefaults } from 'vitest/config';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    ...configDefaults,
+
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setup.test.ts', // Ensure this file exists for setting up Vitest
+  },
+});
