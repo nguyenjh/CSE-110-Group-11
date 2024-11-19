1ac3f759f9daf9a63fd76a488e7d71715271286e
hoangle2404 <146885173+hoangle2404@users.noreply.github.com>
2024-11-19 13:42:57 -0800

    add function for like recipe button and like comment button

---
+++ my-app/client/src/components/RecipeContent/RatingStars.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { L } from "vitest/dist/chunks/reporters.anwo7Y6a.js";
+
+const default_count = 5;
+const default_symbol = '✭';
+const default_unselected_color = 'grey';
+const default_selected_color = 'yellow';
+
+export default function RatingStars({count, rating, symbol, color, symbolSize}) {
+
+}
