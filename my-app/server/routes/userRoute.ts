6f5ab2fbabea393411f8b087a64c789463f16c7e
ganadenjameel <116318527+jameelganaden@users.noreply.github.com>
2024-11-19 23:59:07 -0800

    Update userRoute.ts

--- my-app/server/routes/userRoute.ts
+++ my-app/server/routes/userRoute.ts
@@ -1,17 +1,17 @@
-// userRoute.ts
-
-import express, { Request, Response } from 'express';
-import { protect } from '../middleware/auth';
+import {protect} from '../middleware/auth';
+import asyncHandler from 'express-async-handler';
+import express, { Request, Response, NextFunction} from 'express';
+import {User} from '../models/User';
+// import express from 'express';
 import { registerUser, loginUser, getMe, getAll } from '../controllers/userController';
 
 const router = express.Router();
-
+router.get('/', getAll);// GET /api/
 // Public routes
-router.post('/register', registerUser);  // POST /api/register
-router.post('/login', loginUser);        // POST /api/login
+router.post('/register', registerUser); // POST /api/register
+router.post('/login', loginUser); // POST /api/login
 
 // Private route
-router.get('/me', protect, getMe);       // GET /api/me
-router.get('/', getAll);                 // Get all users (for testing purposes)
+router.get('/me', protect, getMe); // GET /api/me
 
 export default router;
