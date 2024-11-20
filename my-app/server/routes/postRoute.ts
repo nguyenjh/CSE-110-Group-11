fb67620deb23a12fc56ac0202c29a43b1cf3e679
ganadenjameel <116318527+jameelganaden@users.noreply.github.com>
2024-11-19 23:58:36 -0800

    Update postRoute.ts

--- my-app/server/routes/postRoute.ts
+++ my-app/server/routes/postRoute.ts
@@ -1,18 +1,67 @@
-// postRoute.ts
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// recipe_route.js
+// Code is modified from MongoDB MERN tutorial
+//
+// Our express router file that will define the behavior between frontend requests(GET, POST, etc.) 
+// and backend responses(relaying requests back to MongoDB Atlas). 
+// Assembled by Alex Paz
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-import express from 'express';
-import { getAllPosts, getPost, addPost } from '../controllers/postController';
+import express from "express"
+import { getAllPosts, getPost, addPost, getFilteredPosts} from "../controllers/postController";
 
 const router = express.Router();
 
-// GET Request for All Posts
+// GET Request for All Recipes: If the route is homepage and a default request was made, send full recipe database.
 router.get("/", getAllPosts);
 
-// GET Request for Specific Post or Filter Posts
-router.get("/:id", getPost); // To get a post by ID
-router.get("/filter", getPost); // To get posts by filter query parameter
+// GET Request for Specific Recipe: If the route is homepage/:id and a default request is made, send the single 
+// recipe with matching id to the database.
+router.get("/:id", getPost);
 
-// POST Request to add a new post
+// POST Request to add recipe to database.
 router.post("/", addPost);
+
+router.get("/filter",getFilteredPosts);
+// Patch Request to Update a Specific Recipe: When on homepage/:id page, compile the form data 
+// from req and update the recipe found from the id param. 
+
+/*router.patch("/:id", async(req,res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id)};
+    const updates = {
+      $set: {
+        name: req.body.name,
+        summary: req.body.summary,
+        instructions: req.body.instructions,
+      },};
+
+    let collection = await db.collection("recipe");
+    let result = await collection.updateOne(query, updates);
+    res.send(result).status(200);
+  }
+
+  catch{
+    console.error(err);
+    res.status(500).send("Error updating recipe");
+  }
+});
+
+// DELETE Request to Delete a Specific Recipe: When on homepage/:id, get the recipe through 
+// the id param and delete the query from the collection.
+router.delete("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id)};
+    const collection = db.collection("recipe");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+  }
+
+  catch(err){
+    console.error(err);
+    res.status(500).send("Error deleting recipe");
+  }
+});*/
 
 export default router;
